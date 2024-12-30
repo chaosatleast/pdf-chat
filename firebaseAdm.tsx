4037e40089f378f7f92c4010fc9a81594ef0bc8d
acxy99 <alicechin9017@gmail.com>
2024-12-30 23:25:04 +0800

    Change service key to service key env

--- firebaseAdm.tsx
+++ firebaseAdm.tsx
@@ -2,16 +2,18 @@ import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-const serviceKey = require("@/service_key.json");
+const serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_KEY as string);
+
+// const serviceKey = require("@/service_key.json");
 
 let app: App;
 
 if (getApps().length === 0) {
-  app = initializeApp({
-    credential: cert(serviceKey),
-  });
+	app = initializeApp({
+		credential: cert(serviceKey),
+	});
 } else {
-  app = getApp();
+	app = getApp();
 }
 
 const adminDb = getFirestore(app);
