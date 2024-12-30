33ee12b2cf56b0af09b257bb38bff309dc658620
acxy99 <alicechin9017@gmail.com>
2024-12-30 23:02:46 +0800

    Increase Timeout

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -5,33 +5,34 @@ import PDFView from "@/custom/PDFView";
 import Chat from "@/custom/Chat";
 import { Toaster } from "@/components/ui/toaster";
 
+export const maxDuration = 60;
 const FilePage = async ({ params: { id } }: { params: { id: string } }) => {
-  auth().protect();
+	auth().protect();
 
-  const { userId } = await auth();
+	const { userId } = await auth();
 
-  const ref = await adminDb
-    .collection("users")
-    .doc(userId!)
-    .collection("files")
-    .doc(id)
-    .get();
+	const ref = await adminDb
+		.collection("users")
+		.doc(userId!)
+		.collection("files")
+		.doc(id)
+		.get();
 
-  const url = ref.data()?.downloadUrl;
+	const url = ref.data()?.downloadUrl;
 
-  return (
-    <div className="grid grid-cols-5 h-full overflow-hidden">
-      {/* PDF View */}
-      <div className="col-span-5 order-last overflow-y-auto lg:order-first lg:col-span-3 dark:bg-zinc-800 overflow-auto">
-        <PDFView url={url} />
-      </div>
-      {/* Chat */}
-      <div className="col-span-5 lg:col-span-2  dark:bg-zinc-900  overflow-auto ">
-        <Toaster />
-        <Chat id={id} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="grid grid-cols-5 h-full overflow-hidden">
+			{/* PDF View */}
+			<div className="col-span-5 order-last overflow-y-auto lg:order-first lg:col-span-3 dark:bg-zinc-800 overflow-auto">
+				<PDFView url={url} />
+			</div>
+			{/* Chat */}
+			<div className="col-span-5 lg:col-span-2  dark:bg-zinc-900  overflow-auto ">
+				<Toaster />
+				<Chat id={id} />
+			</div>
+		</div>
+	);
 };
 
 export default FilePage;
